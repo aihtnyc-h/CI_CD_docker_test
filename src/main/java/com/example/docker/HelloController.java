f35d00b2aec9cabeb1e66cc7308f0e1e49f921b9
aihtnyc_h <feiver@naver.com>
2023-03-15 14:46:38 +0900

    feat : test

--- src/main/java/com/example/docker/HelloController.java
+++ src/main/java/com/example/docker/HelloController.java
@@ -1,2 +1,12 @@
-package com.example.docker;public class HelloController {
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping(("/hello"))
+	public String hello(){
+		return "hello";
+	}
 }
